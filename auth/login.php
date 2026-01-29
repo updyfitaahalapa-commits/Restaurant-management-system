17cd67087a59916b3a325fca51dcd89546bfe80b
updyfitaahalapa-commits <updyfitaahalapa@gmail.com>
2026-01-29 03:20:39 +0300

    commit

--- auth/login.php
+++ auth/login.php
@@ -45,7 +45,7 @@ if (isset($_POST['login'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sign In | Gourmet Haven</title>
+    <title>Sign In | Kaama Liito</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
